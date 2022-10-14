556ff84c416c1a7c9333dd556b56e3393b1d2ad4
tranlamm <tranlam021102eee@gmail.com>
2022-10-14 23:55:52 +0700

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:80';
+const URL = 'https://job-app-tranlamm.herokuapp.com';
 
 export const fetchJobs = (payload) => axios.get(`${URL}/jobs`, { params: { page: payload } });
 export const fetchSearchJobs = (payload) => axios.get(`${URL}/jobs/search`, { params: { keyword: payload } });
